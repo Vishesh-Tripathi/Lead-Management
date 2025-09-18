5f696a865b46acea7fd58448f6fbf8a2ca585207
Vishesh Tripathi <tripathiishu467@gmail.com>
2025-09-18 17:46:27 +0530

    d

--- Backend/index.js
+++ Backend/index.js
@@ -14,10 +14,7 @@ import leadRoutes from "./src/routes/leads.js";
 const app = express();
 
 // Middleware
-app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
